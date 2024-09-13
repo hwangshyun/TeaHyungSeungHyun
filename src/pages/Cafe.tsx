173e240e0c06a933ad425b82aa6b4403be9330a4
hwangseunghyun <hwamg123@naver.com>
2024-09-13 15:21:35 +0900

    라우터설정

---
+++ src/pages/Cafe.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cafe = () => {
+  return (
+    <div>카페</div>
+  )
+}
+
+export default Cafe
---
+++ src/pages/Restaurant.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Restaurant = () => {
+  return (
+    <div>일반식당</div>
+  )
+}
+
+export default Restaurant
